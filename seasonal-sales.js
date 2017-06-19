82f0d5e4f9536078e47c60a1f4749818ff0709d3
Dustin Walker <thewalkman1234@gmail.com>
2017-06-19 16:03:04 -0500

    built the main js page to stick stuff on a card

---
+++ seasonal-sales.js
@@ -0,0 +1,69 @@
+console.log("hello");
+
+
+
+let products = null;
+let categories = null;
+
+function buildDOMObj() {
+	//loop through products and categories to grab Prd name, Dpt, Price and cat ID
+	let productArr = products.map( function(product) {
+		//inside this loop we need to loop again, but this time through the categories 
+		//array to find the one category whose id matches the category id of the currentPrduct
+		//maybe .filer()
+		//the returned array will contain one object. can set "Dept" on the new obj
+		//we are making the name propery of that one object
+		let categoryItem = categories.filter( function(category) {
+			return category.id === currentProduct.category_id;
+		})
+		let prodObj = {dept: categoryItem[0].name}
+		return prodObj 
+	})
+	console.log("prod object", productArr)
+
+}
+
+function buildCard(prodObj) {
+	let card = `<div class="prodCard">
+					<h2>${prodObj.name}</h2>
+					<h3>${prodObj.dept}</h3>
+					<p>${prodObj.price}</p>
+					</div>`;
+	return card;
+}
+let TempObj = {name: "furby",dept: "Toys", price: 12.75}
+console.log("card", buildCard(TempObj));
+
+
+function displayProducts() {
+	products = JSON.parse(productsJSON).products;
+}
+
+function setCategoriess() {
+	categories = JSON.parse(event.target.responseText).categories;
+	buildDOMObj
+}
+
+function getCategories() {
+	let reqCats = new XMLHttpRequest();
+	reqCats.addEventListener("load", setCategoriess);
+	reqCats.open("GET", "data-files/categories.json");
+	reqCats.send();
+}
+
+function getProducts() {
+	let reqProducts = new XMLHttpRequest();
+	reqProducts.addEventListener("load", setProducts);
+	reqProducts.open("GET", "data-files/products.json");
+	reqProducts.send();
+}
+
+getProducts();
+
+// getCategories();
+
+
+//data attributes
+
+
+
